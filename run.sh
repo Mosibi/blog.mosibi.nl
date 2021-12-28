07190876687bb94816c274ce59763d264824f80d
Richard Arends <richard@unixguru.nl>
2021-12-28 11:52:57 +0100

    Fix permission errors

--- run.sh
+++ run.sh
@@ -3,6 +3,9 @@
 #toolbox run --container jekyll bundle exec jekyll serve
 export JEKYLL_VERSION=3.8
 
+rm "${PWD}/Gemfile.lock"
+touch "${PWD}/Gemfile.lock"
+chmod 666 "${PWD}/Gemfile.lock"
 podman run --rm \
   --volume="$PWD:/srv/jekyll:z" \
   --publish [::1]:4000:4000 \
